391221e960cb51fc2e503fdd4cc3b5a9347db29f
UMGZS <umgzs01@gmail.com>
2020-05-25 12:08:16 +0800

    umweb v0.3

--- pages/paper/paper.vue
+++ pages/paper/paper.vue
@@ -1,22 +1,151 @@
 <template>
-	<view>
-		
+	<view class="paper-body">
+		<view class="paper-left-popup">
+			<view>
+				<view class=""></view>加好友
+			</view>
+			<view>
+				<view></view>清理缓存
+			</view>
+		</view>
+		<block v-for="(item,index) in list" :key="index">
+			<paper-list :item="item" :index="index"></paper-list>
+		</block>
+		<load-more :loadtext="loadtext"></load-more>
 	</view>
 </template>
 
 <script>
+	import paperList from '../../components/paper-list/paper-list.vue'
+	import loadMore from "../../components/load-more/load-more.vue"
 	export default {
 		data() {
 			return {
-				
+				loadtext: "上拉加载更多",
+				list: [{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "0"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					},
+					{
+						userpic: "../../static/image/user.jpg",
+						username: "kolboy",
+						time: "10:45",
+						data: "我是信息",
+						nornum: "12"
+					}
+				]
 			}
 		},
+		onReachBottom() {
+			this.loadData();
+		},
+		onPullDownRefresh() {
+			this.PullRefresh();
+		},
 		methods: {
-			
+			PullRefresh() {
+				setTimeout(() => {
+
+					let arr = [
+
+						{
+							userpic: "../../static/image/user.jpg",
+							username: "kolman",
+							time: "10:45",
+							data: "我是信息",
+							nornum: "12"
+						},
+						{
+							userpic: "../../static/image/user.jpg",
+							username: "boxk",
+							time: "10:45",
+							data: "我是信息",
+							nornum: "0"
+						},
+						{
+							userpic: "../../static/image/user.jpg",
+							username: "kolboy",
+							time: "10:45",
+							data: "我是信息",
+							nornum: "12"
+						}
+					];
+					this.list = arr;
+					uni.stopPullDownRefresh();
+				}, 2000)
+			},
+			loadData() {
+				if (this.loadtext != "上拉加载更多") {
+					return;
+				}
+				this.loadtext = "加载中...";
+			setTimeout(() => {
+				let obj = {
+					userpic: "../../static/image/user.jpg",
+					username: "openbox",
+					time: "10:45",
+					data: "我是信息",
+					nornum: "12"
+				};
+				this.list.push(obj);
+				this.loadtext = "上拉加载更多";
+			}, 2000);
 		}
+	},
+	components: {
+		paperList,
+		loadMore
+	}
 	}
 </script>
 
 <style>
-
+	.paper-body {
+		padding: 0upx 20upx;
+	}
 </style>
--- pages/topicDetail/topicDetail.vue
+++ pages/topicDetail/topicDetail.vue
@@ -8,6 +8,7 @@
 					<block v-for="(list,listindex) in item.list" :key="listindex">
 						<comon-list :item="list" :index="listindex"></comon-list>
 					</block>
+					<load-more :loadtext="item.loadtxt"></load-more>
 				</template>
 			</block>
 		</view>
@@ -18,6 +19,7 @@
 	import topicInfo from '../../components/topic-info/topic-info.vue'
 	import swiperHeaderBox from "../../components/swiper-header-box/swiper-header-box.vue"
 	import comonList from "../../components/comon-list/comon-list.vue"
+	import loadMore from "../../components/load-more/load-more.vue"
 	export default {
 		data() {
 			return {
@@ -40,7 +42,7 @@
 					newnum: 100
 				},
 				tablist: [{
-						loadtxt: "上拉加载更多",
+						loadtext: "上拉加载更多",
 						list: [
 							{
 								userpic: "../../static/image/user.jpg",
@@ -114,7 +116,7 @@
 						]
 					},
 					{
-						loadtxt: "上拉加载更多",
+						loadtext: "上拉加载更多",
 						list: [
 							{
 								userpic: "../../static/image/user.jpg",
@@ -193,15 +195,124 @@
 		onLoad() {
 			
 		},
+		onReachBottom() {
+			this.loadmore();
+		},
+		onPullDownRefresh() {
+			this.pullrefresh();
+		},
 		methods: {
 			tabtap(index) {
 				this.tabIndex = index;
+			},
+			loadmore() {
+						if(this.tablist[this.tabIndex].loadtext != "上拉加载更多"){
+							return ;
+						}
+						this.tablist[this.tabIndex].loadtext = "加载中...";
+						setTimeout(()=> {
+							let obj = {
+										userpic: "../../static/image/user.jpg",
+										username: "oooo",
+										sex: 0,//0 男 1 女
+										age: 25,
+										isguanzhu: false,
+										title: "我是标题",
+										titlepic: "",
+										video: false,
+										share: false,
+										path: "深圳 龙岗",
+										sharenum: 20,
+										commentnum: 30,
+										goodnum: 20
+									};
+							this.tablist[this.tabIndex].list.push(obj);
+							this.tablist[this.tabIndex].loadtext = "上拉加载更多";
+						},2000);
+			},
+			pullrefresh() {
+				setTimeout(()=>{
+					let arr = [
+							{
+								userpic: "../../static/image/user.jpg",
+								username: "dsadsads",
+								sex: 0,//0 男 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是标题ss",
+								titlepic: "",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							//图文
+							{
+								userpic: "../../static/image/user.jpg",
+								username: "kolboy",
+								sex: 1,//0 男 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是标题",
+								titlepic: "../../static/image/index01.jpg",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							//视频
+							{
+								userpic: "../../static/image/user.jpg",
+								username: "kolboy",
+								sex: 0,//0 男 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是标题",
+								titlepic: "../../static/image/index01.jpg",
+								video: {
+									looknum: "28w",
+									long: "2:47"
+								},
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							//分享
+							{
+								userpic: "../../static/image/user.jpg",
+								username: "kolboy",
+								sex: 1,//0 男 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是标题",
+								titlepic: "",
+								video: false,
+								share: {
+									title: "我是分享标题",
+									titlepic: "../../static/image/index01.jpg"
+								},
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							}
+					]
+					this.tablist[this.tabIndex].list = arr
+					uni.stopPullDownRefresh();
+				},2000);
 			}
 		},
 		components: {
 			topicInfo,
 			swiperHeaderBox,
-			comonList
+			comonList,
+			loadMore
 		}
 	}
 </script>
